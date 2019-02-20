325686d484758ef897bb507988c2c73d56e89f06
Julio Benavente <32945829+julio-benavente@users.noreply.github.com>
2019-02-20 13:14:38 -0500

    Almost finished

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// $(function () {
+// 	alert('hello');
+// })
+
+
+
+// $(function() { alert('hello') });
+
+$(function () {
+	$('#projects').tabs();
+});
+
+
+var project-button = 
+
+$().button('toggle')
